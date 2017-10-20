d1245708db20d9697013740baf489cb86a16ca6a
now&here <12541348@qq.com>
2017-10-20 16:01:52 +0800

    Initial commit

---
+++ CHTWaterfallSwiftDemo/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  CHTWaterfallSwift
+//
+//  Created by Sophie Fader on 3/21/15.
+//  Copyright (c) 2015 Sophie Fader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, CHTCollectionViewDelegateWaterfallLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let model = Model()
+
+    
+    //MARK: - View Controller Lifecycle
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        model.buildDataSource()
+        
+        
+        // Attach datasource and delegate
+        self.collectionView.dataSource  = self
+        self.collectionView.delegate = self
+        
+        //Layout setup
+        setupCollectionView()
+        
+        //Register nibs
+        registerNibs()
+    }
+   
+    
+    //MARK: - CollectionView UI Setup
+    func setupCollectionView(){
+        
+        // Create a waterfall layout
+        let layout = CHTCollectionViewWaterfallLayout()
+        
+        // Change individual layout attributes for the spacing between cells
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        layout.minimumColumnSpacing = 10.0
+        layout.minimumInteritemSpacing = 10.0
+        
+        // Collection view attributes
+        self.collectionView.autoresizingMask = [UIViewAutoresizing.flexibleHeight, UIViewAutoresizing.flexibleWidth]
+        self.collectionView.alwaysBounceVertical = true
+        
+        // Add the waterfall layout to your collection view
+        self.collectionView.collectionViewLayout = layout
+    }
+    
+    // Register CollectionView Nibs
+    func registerNibs(){
+        
+        // attach the UI nib file for the ImageUICollectionViewCell to the collectionview 
+        let viewNib = UINib(nibName: "ImageUICollectionViewCell", bundle: nil)
+        collectionView.register(viewNib, forCellWithReuseIdentifier: "cell")
+    }
+    
+    
+    
+    
+    //MARK: - CollectionView Delegate Methods
+    
+     //** Number of Cells in the CollectionView */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model.images.count
+    }
+    
+    
+    //** Create a basic CollectionView Cell */
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Create the cell and return the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageUICollectionViewCell
+        
+        // Add image to cell
+        cell.image.image = model.images[indexPath.row]
+        return cell
+    }
+    
+    
+    //MARK: - CollectionView Waterfall Layout Delegate Methods (Required)
+    
+    //** Size for the cells in the Waterfall Layout */
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
+        // create a cell size from the image size, and return the size
+        let imageSize = model.images[indexPath.row].size
+        
+        return imageSize
+    }
+}
+
